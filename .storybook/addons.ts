fbc6632116dc5df7c45f0281e62da9086bf4bfa7
goldsteinsveta <svetlana.goldstein@gmail.com>
2020-06-19 19:54:42 +0200

    feat: storybook ts config

---
+++ .storybook/addons.ts
@@ -0,0 +1,8 @@
+import '@storybook/addon-actions/register';
+import '@storybook/addon-docs/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-links/register';
+import 'storybook-addon-theme-playground/dist/register';
+
+// TODO:
+import '@storybook/addon-storysource/register';
---
+++ .storybook/config.ts
@@ -0,0 +1,29 @@
+import 'semantic-ui-css/semantic.min.css';
+import { configure } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { withThemePlayground } from 'storybook-addon-theme-playground';
+import { polkadot, substrate } from '../src/globalStyle';
+
+const req = require.context('../stories', true, /.stories.tsx$/);
+function loadStories () {
+  req.keys().forEach(req);
+}
+configure(loadStories, module);
+
+const options = {
+  theme: [
+    { name: 'Polkadot', theme: polkadot },
+    { name: 'Substrate', theme: substrate }
+  ],
+  provider: ThemeProvider,
+  overrides: {},
+  config: {
+    labelFormat: 'path',
+    showCode: true,
+    debounce: true,
+    debounceRate: 500,
+  }
+};
+addDecorator(withThemePlayground(options));
+
